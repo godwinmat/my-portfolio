d129b343d6b562b91dde25ef2ee8f5d58b1154be
Matthew Godwin <67100329+godwinmat@users.noreply.github.com>
2023-05-31 15:57:10 +0100

    Reduced the font size of the CustomButton text

--- app/components/CustomButton.js
+++ app/components/CustomButton.js
@@ -9,7 +9,7 @@ const CustomButton = ({ href = "", children }) => {
             className="my-4 inline-block bg-primary rounded-[4px]"
         >
             <button
-                className={`${roboto.className} border border-primary text-primary text-base py-[18px] px-12 rounded-[4px] hover:-translate-x-[6px] hover:-translate-y-[6px] bg-background duration-300`}
+                className={`${roboto.className} border border-primary text-primary text-sm py-[18px] px-12 rounded-[4px] hover:-translate-x-[6px] hover:-translate-y-[6px] bg-background duration-300`}
             >
                 {children}
             </button>
